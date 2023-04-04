e805d4d3e52b47b8dbb7303b08d9f47eed6d4469
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2023-04-04 16:28:59 +0700

    responsive navbar

--- components/navigators/Navbar.tsx
+++ components/navigators/Navbar.tsx
@@ -19,16 +19,16 @@ const Navbar: NextComponentType<NextPageContext, {}, Props> = (
 
   return (
     <nav className="flex justify-center bg-transparent text-zinc-900 fixed w-full">
-      <div className="w-10/12 flex flex-row justify-between p-4 items-center">
+      <div className="w-full lg:w-10/12 flex flex-row justify-between px-4 py-2 lg:py-4 items-center">
         <Image
           src={LogoProfile}
-          className="w-10 h-10 rounded-full drop-shadow-sm"
+          className="w-8 h-8 lg:w-10 lg:h-10 rounded-full drop-shadow-sm"
           height={0}
           width={0}
           alt="Logo Profile"
         />
-        <div className="flex flex-row space-x-8 items-center">
-          <ul className="flex flex-row space-x-12">
+        <div className="flex flex-row space-x-4 md:space-x-8 items-center">
+          <ul className="flex flex-row space-x-2 md:space-x-4 lg:space-x-12">
             <li
               role="button"
               className={
@@ -82,31 +82,31 @@ const Navbar: NextComponentType<NextPageContext, {}, Props> = (
               Contact
             </li>
           </ul>
-          <div className="flex flex-row space-x-4">
+          <div className="flex flex-row space-x-2 lg:space-x-4 items-center">
             <a
               role="button"
-              className="border-2 rounded-full p-2 border-instagram"
+              className="border-2 rounded-full p-1 lg:p-2 border-instagram"
               target="_blank"
               href="https://instagram.com/tondikii?igshid=ZDdkNTZiNTM="
             >
               <Image
                 src="/icon-instagram.svg"
                 alt="Instagram icon"
-                className="w-7 h-7"
+                className="w-5 lg:w-7 w-5 lg:h-7"
                 width={0}
                 height={0}
               />
             </a>
             <a
               role="button"
-              className="border-2 rounded-full p-2 border-linkedin"
+              className="border-2 rounded-full p-1 lg:p-2 border-linkedin"
               target="_blank"
               href="https://www.linkedin.com/in/tondikii/"
             >
               <Image
                 src="/icon-linkedin.svg"
                 alt="Linkedin icon"
-                className="w-7 h-7"
+                className="w-5 lg:w-7 w-5 lg:h-7"
                 width={0}
                 height={0}
               />
